a63cd69caf2ff95a789a729bd666b2a344142803
Tadeu Nascimento <47820260+TadeunASCIImento@users.noreply.github.com>
2021-06-11 21:19:20 -0300

    Primeiro commit (Arrays).

---
+++ src/br/com/revisao/arrays/TesteArrays01.java
@@ -0,0 +1,27 @@
+package br.com.revisao.arrays;
+
+public class TesteArrays01 {
+
+	public static void main(String[] args) {
+		Integer[] array = createIntegerArray();
+		showIntegerArray(array);
+	}
+
+	private static Integer[] createIntegerArray() {
+		// constante que define o tamanho o do array.
+		final int ARRAY_LENGTH = 10;
+		Integer[] arrayInteger = new Integer[ARRAY_LENGTH];
+		for (int index = 0; index < arrayInteger.length; index++) {
+			arrayInteger[index] = (int) Math.pow(index, 2);
+		}
+		return arrayInteger;
+	}
+
+	private static void showIntegerArray(Integer[] integerArray) {
+		System.out.print("Elementos no array: ");
+		for (Integer integer : integerArray) {
+			System.out.printf("%d ", integer);
+		}
+	}
+
+}
