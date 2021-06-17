c231d8ab12de7e635430df63b52920dc8a7a401f
Tadeu Nascimento <47820260+TadeunASCIImento@users.noreply.github.com>
2021-06-16 22:24:05 -0300

    Melhorando o código da classe.

--- src/br/com/revisao/arrays/TesteArrays04.java
+++ src/br/com/revisao/arrays/TesteArrays04.java
@@ -8,54 +8,55 @@ import java.security.SecureRandom;
 
 public class TesteArrays04 {
 
-	private static final Integer ARRAY_LENGTH = 10; // define o tamanho do array.
+	private static Integer size = 2; // define o tamanho dos arrays.
 
 	public static void main(String[] args) {
 
-		Integer total = 0;
-		Integer[] arrayInteger;
-		// gera dez arrays de números inteiros aleatórios.
-		for (int index = 0; index < ARRAY_LENGTH; index++) {
-			arrayInteger = generateArrayInteger(ARRAY_LENGTH);
-			showArraysNumbers(arrayInteger);
-			total += sumArrayIntegersElements(arrayInteger);
-			showMessage("\n");
-		}
-		System.out.printf("Resultado da soma de todos os arrays: %d", total);
+		// Gera e exibe dois arrays de números inteiros.
+		imprimiAsMensagensNoConsole("Elementos no array1:");
+		Integer[] arrayDeInteiros1 = gerarArrayDeInteiros(size);
+		exibirElementosNoArray(arrayDeInteiros1);
+		
+		imprimiAsMensagensNoConsole("Elementos no array2:");
+		Integer[] arrayDeInteiros2 = gerarArrayDeInteiros(size);
+		exibirElementosNoArray(arrayDeInteiros2);
 
+		// Exibe a soma de todos elementos do array1 com array2 
+		Integer somaTotalArrays = somarTodosElementosDoArray(arrayDeInteiros1, arrayDeInteiros2);
+		imprimiAsMensagensNoConsole("Soma de todos elementos dos arrays:".concat(somaTotalArrays.toString()));
 	}
 
-	// Exibe os números nos dez arrays gerados aleatóriamente.
-	private static void showArraysNumbers(Integer[] arrayInteger) {
-		for (Integer integer : arrayInteger) {
-			System.out.printf("%d%s ", integer, " ");
-		}
-
-	}
-
-	// Soma todos elementos dos arrays de números inteiros.
-	private static Integer sumArrayIntegersElements(Integer[] arrayInteger) {
+	// Soma os arrays e retorna o resultado..
+	private static Integer somarTodosElementosDoArray(Integer[]... arrays) {
 		Integer total = 0;
-		for (Integer integer : arrayInteger) {
-			total += integer;
+		for (int index = 0; index < size; index++) {
+			total += arrays[0][index] + arrays[1][index];
 		}
 		return total;
 	}
 
-	// gera um array de números inteiros aleatóriamente.
-	private static Integer[] generateArrayInteger(Integer arrayLenght) {
-		SecureRandom secureRandom = new SecureRandom();
-		Integer[] arrayInteger = new Integer[arrayLenght];
-		Integer seed = 100;
-		for (int index = 0; index < arrayLenght; index++) {
-			arrayInteger[index] = secureRandom.nextInt(seed);
+	// Exibe os elementos nos arrays.
+	private static void exibirElementosNoArray(Integer[] array) {
+		for (Integer integer : array) {
+			System.out.printf("%d ", integer);
 		}
-		return arrayInteger;
+		System.out.print("\n");
+
 	}
 
 	// Exibe as mensagens no console.
-	private static void showMessage(String message) {
-		System.out.print(message);
+	private static void imprimiAsMensagensNoConsole(Object mensagem) {
+		System.out.print(mensagem);
 	}
 
+	// Gera um array de inteiros aleatórios com o tamanho definido em size. 
+	private static Integer[] gerarArrayDeInteiros(Integer size) {
+		Integer seed = 100;// define uma semente para os elementos no array.
+		SecureRandom geradorRandomicoSeguro = new SecureRandom();
+		Integer[] arrayInteiros = new Integer[size];
+		for (int index = 0; index < arrayInteiros.length; index++) {
+			arrayInteiros[index] = 1 + geradorRandomicoSeguro.nextInt(seed);
+		}
+		return arrayInteiros;
+	}
 }
